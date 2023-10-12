9686299d1e074906aeee0801ce8bc24432d8e17d
Devin Basley <Devinbasley@gmail.com>
2023-10-12 18:35:15 -0400

    Includes "Honeywell" on vertical Line

--- Honeywell_Followers_boxplot.R
+++ Honeywell_Followers_boxplot.R
@@ -1,14 +1,16 @@
 library(tidyverse)
 
-companies$honeywell <- ifelse(companies$company_id == 1344, TRUE, FALSE)
+
 
 companies %>%
   ggplot(aes(y = log(followers))) +
   #geom_violin() +
   geom_boxplot(aes(x = "")) +
   geom_hline(yintercept = 14.75, color = "red") +
+  annotate("text", x = 1.2, y = 14, label="Honeywell", angle = 90) +
   coord_flip() +
   labs(x = "",
        y = "Log Linkedin Followers",
        title = "Number of Linkedin Followers for Honeywell") +
   theme_bw()
+  #facet_wrap(~ industry)
