1b35d6d5352a7024d069dda19b1f9d896989942c
Devin Basley <Devinbasley@gmail.com>
2023-10-12 10:26:16 -0400

    Update Stock Price Visual Highlighting Honeywell

--- Daily_Stock_Price.R
+++ Daily_Stock_Price.R
@@ -11,40 +11,35 @@ booz_allen <- tq_get(c("BAH"), get  = "stock.prices",
 verizon <- tq_get(c("VZ"), get  = "stock.prices",
                   from = "2015-01-01", to = "2023-10-01")
 
-# Stock Price for Robert Half
-robert_half <- tq_get(c("RHI"), get  = "stock.prices",
-                      from = "2015-01-01", to = "2023-10-01")
-
-
-# Stock Price for Truist
-truist <- tq_get(c("TFC"), get  = "stock.prices",
-                 from = "2015-01-01", to = "2023-10-01")
 
 # Stock price for Honeywell
 honeywell <- tq_get(c("HON"), get  = "stock.prices",
                     from = "2015-01-01", to = "2023-10-01")
 
-# Stock Price for MasterCard
-mastercard <- tq_get(c("MA"), get  = "stock.prices",
-                     from = "2015-01-01", to = "2023-10-01")
 
 # Stock Price for Apex Systems
 apex_systems <- tq_get(c("ASGN"), get  = "stock.prices",
                        from = "2015-01-01", to = "2023-10-01")
 
-
 # Combine all Company Stock Prices into 1 data frame
-combine_stocks <- rbind(booz_allen, verizon, robert_half, truist, honeywell, mastercard, apex_systems)
+combine_stocks <- rbind(booz_allen, verizon, honeywell, apex_systems)
 
 
+
+  
 
 
 # Plot the daily closing stock price of each company Jan 1, 2015 to October 1, 2023
 combine_stocks %>%
-  ggplot(aes(x = date, y = close, color = symbol)) +
-  geom_line(alpha = 0.5) +
+  filter(symbol != "HON") %>%
+  ggplot() +
+  geom_line(aes(x = date, y = close, group = symbol), 
+            color = "gray", 
+            alpha = 0.5) +
+  geom_line(data = filter(combine_stocks, symbol == "HON"), 
+            aes(x = date, y = close, color = symbol), alpha = 1) +
   labs(x = "Date",
        y = "Close Price of Stock",
-       title = "Closing Stock Price of Company from 2015-2023",
-       color = "Company Ticker Symbol") +
-  theme_bw()
+       title = "Closing Stock Price of Company from 2015-2023") +
+  theme_bw() +
+  theme(panel.grid = element_blank())
