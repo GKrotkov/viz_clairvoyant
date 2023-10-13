9f36c32a7452bbb17633ac0492bf86ea82e53dd5
Gabriel Kroktov <GKrotkov@users.noreply.github.com>
2023-10-13 11:59:42 -0400

    Pushing final day project results

--- Daily_Stock_Price.R
+++ Daily_Stock_Price.R
@@ -21,35 +21,20 @@ honeywell <- tq_get(c("HON"), get  = "stock.prices",
 apex_systems <- tq_get(c("ASGN"), get  = "stock.prices",
                        from = "2015-01-01", to = "2023-10-01")
 
-# Stock price for IBM
-ibm <- tq_get(c("IBM"), get  = "stock.prices",
-              from = "2015-01-01", to = "2023-10-01")
-
-# Stock Price for Oracle
-oracle <- tq_get(c("ORCL"), get  = "stock.prices",
-                 from = "2015-01-01", to = "2023-10-01")
-
 # Combine all Company Stock Prices into 1 data frame
-combine_stocks <- rbind(booz_allen, verizon, honeywell, apex_systems, ibm, oracle)
-
-grouped_stocks <- combine_stocks %>%
-  filter(symbol != "HON")
-
-
-
+combine_stocks <- rbind(booz_allen, verizon, honeywell, apex_systems)
 
 # Plot the daily closing stock price of each company Jan 1, 2015 to October 1, 2023
 combine_stocks %>%
   filter(symbol != "HON") %>%
   ggplot() +
   geom_line(aes(x = date, y = close, group = symbol), 
-            alpha = 0.5, color = "gray") +
+            color = "gray", 
+            alpha = 0.5) +
   geom_line(data = filter(combine_stocks, symbol == "HON"), 
             aes(x = date, y = close, color = symbol), alpha = 1) +
   labs(x = "Date",
        y = "Close Price of Stock",
-       title = "Honeywell Stock Outperforms Competitors from 2015-2023",
-       color = "Ticker Symbol",
-       subtitle = "Includes Verizon, Oracle, Booz Allen Hamilton, IBM, Apex Systems") +
+       title = "Closing Stock Price of Company from 2015-2023") +
   theme_bw() +
   theme(panel.grid = element_blank())
--- Honeywell_Followers_boxplot.R
+++ Honeywell_Followers_boxplot.R
@@ -1,33 +1,14 @@
 library(tidyverse)
 
-
+companies$honeywell <- ifelse(companies$company_id == 1344, TRUE, FALSE)
 
 companies %>%
   ggplot(aes(y = log(followers))) +
   #geom_violin() +
   geom_boxplot(aes(x = "")) +
   geom_hline(yintercept = 14.75, color = "red") +
-  annotate("text", x = 1.1, y = 15.5, label="Honeywell", angle = 90, size = 2.5) +
   coord_flip() +
   labs(x = "",
        y = "Log Linkedin Followers",
-       title = "Honeywell Has More Linkedin Followers Than Other Companies",
-       subtitle = "Faceted by Company Size: 1 being Small, 7 being Large") +
-  theme_bw() +
-  facet_wrap(~ company_size)
-
-
-# Salary Boxplot, replacing followers
-companies %>%
-  ggplot(aes(y = log(mean_salary_min))) +
-  #geom_violin() +
-  geom_boxplot(aes(x = "")) +
-  geom_hline(yintercept = 14.75, color = "red") +
-  annotate("text", x = 1.1, y = 15.5, label="Honeywell", angle = 90, size = 2.5) +
-  coord_flip() +
-  labs(x = "",
-       y = "Log Linkedin Followers",
-       title = "Honeywell Has a Higher Mean Minimum Salary than Other Companies",
-       subtitle = "Faceted by Company Size: 1 being Small, 7 being Large ") +
-  theme_bw() +
-  facet_wrap(~ company_size)
+       title = "Number of Linkedin Followers for Honeywell") +
+  theme_bw()
